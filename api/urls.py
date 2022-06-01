eee162c30ae374b3470a9b604d4398d728b6602a
p3rplexed _ <102179895+Js-cook@users.noreply.github.com>
2022-06-01 22:13:30 +0000

    ADD - Major changes to api views and urls

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.api_overview, name="overview"),
-    path("retrieve-stats", views.stats, name="stats"),
-    path("add-correct", views.add_correct, name="add-correct"),
-    path("add-incorrect", views.add_incorrect, name="add-incorrect"),
-    path("change-points", views.change_points, name="change-points"),
-    path("retrieve-question", views.retrieve, name="retrieve"),
-    path("retrieve-others", views.retrieve_others, name="retrieve-others")
+    path("retrieve-stats/<str:username>/", views.ind_stats, name="stats"),
+    path("retrieve-all-stats/", views.all_stats, name="all-stats"),
+    path("add-correct/<str:username>/", views.add_correct, name="add-correct"),
+    path("add-incorrect/<str:username>/", views.add_incorrect, name="add-incorrect"),
+    path("change-points/<str:username>/", views.change_points, name="change-points"),
+    path("retrieve-question/", views.retrieve, name="retrieve"),
+    path("retrieve-others/", views.retrieve_others, name="retrieve-others")
 ]
--- api/views.py
+++ api/views.py
@@ -3,22 +3,58 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Question, UserExtension
+
+from .serializers import UserExtensionSerializer, QuestionSerializer
+
 # Create your views here.
 @api_view(["GET"])
 def api_overview(request):
   api_urls = {
-    'Retrieve Stats': '/retrieve-stats',
-    'Add Correct': '/add-correct',
-    'Add Incorrect': '/add-incorrect',
-    'Change Points': '/change-points',
-    'Retrieve Question': '/retrieve-question',
-    'Retrieve Incorrect': '/retrieve-others'
+    'Retrieve User Stats': '/retrieve-stats/<str:username>/',
+    'Retrieve All Stats': '/retrieve-all-stats/',
+    'Add Correct': '/add-correct/<str:username>/',
+    'Add Incorrect': '/add-incorrect/<str:username>/',
+    'Change Points': '/change-points/<str:username>/',
+    'Retrieve Question': '/retrieve-question/',
+    'Retrieve Incorrect': '/retrieve-others/'
   }
   return Response(api_urls)
 
-# path("retrieve-stats", views.stats, name="stats"),
-#     path("add-correct", views.add_correct, name="add-correct"),
-#     path("add-incorrect", views.add_incorrect, name="add-incorrect"),
-#     path("change-points", views.change_points, name="change-points"),
-#     path("retrieve-question", views.retrieve, name="retrieve"),
-#     path("retrieve-others", views.retrieve_others, name="retrieve-others")
+@api_view(["GET"])
+def ind_stats(request, username):
+  extension = UserExtension.objects.get(username=username)
+  serializer = UserExtensionSerializer(extension, many=False)
+  return Response(serializer.data)
+
+@api_view(["GET"])
+def all_stats(request):
+  all_extensions = UserExtension.objects.all()
+  serializer = UserExtensionSerializer(all_extensions, many=True)
+  return Response(serializer.data)
+  
+@api_view(["GET"])
+def add_correct(request, username):
+  user = UserExtension.objects.get(username=username)
+  user.correct += 1
+  serializer = UserExtensionSerializer(user, many=False)
+  return Response(serializer.data)
+
+@api_view(["GET"])
+def add_incorrect(request, username):
+  user = UserExtension.objects.get(username=username)
+  user.incorrect += 1
+  serializer = UserExtensionSerializer(user, many=False)
+  return Response(serializer.data)
+
+@api_view(["POST"])
+def change_points(request):
+  pass
+
+@api_view(["GET"])
+def retrieve(request):
+  pass
+
+@api_view(["GET"])
+def retrieve_others(request):
+  pass
