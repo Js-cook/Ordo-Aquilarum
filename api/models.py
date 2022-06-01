777fe3d68b3a33274d4b5b3cf1b977fcdb5b3d48
p3rplexed _ <102179895+Js-cook@users.noreply.github.com>
2022-06-01 01:28:09 +0000

    ADD - Setup some models and other misc changes

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+  term = models.CharField(max_length=30)
+  case = models.CharField(max_length=20)
+  number = models.CharField(max_length=5)
+  gender = models.CharField(max_length=5)
+  declension = models.CharField(max_length=15)
+
+  def __str__(self):
+    return(self.term)
