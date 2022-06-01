b52a416a981859bd82e201db711963efc9ce903e
p3rplexed _ <102179895+Js-cook@users.noreply.github.com>
2022-06-01 21:07:18 +0000

    ADD - Setup API urls and overview function

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("overview", views.api_overview, name="overview")
+    path("", views.api_overview, name="overview"),
+    path("retrieve-stats", views.stats, name="stats"),
+    path("add-correct", views.add_correct, name="add-correct"),
+    path("add-incorrect", views.add_incorrect, name="add-incorrect"),
+    path("change-points", views.change_points, name="change-points"),
+    path("retrieve-question", views.retrieve, name="retrieve"),
+    path("retrieve-others", views.retrieve_others, name="retrieve-others")
 ]
--- api/views.py
+++ api/views.py
@@ -7,5 +7,18 @@ from rest_framework.response import Response
 @api_view(["GET"])
 def api_overview(request):
   api_urls = {
-    
+    'Retrieve Stats': '/retrieve-stats',
+    'Add Correct': '/add-correct',
+    'Add Incorrect': '/add-incorrect',
+    'Change Points': '/change-points',
+    'Retrieve Question': '/retrieve-question',
+    'Retrieve Incorrect': '/retrieve-others'
   }
+  return Response(api_urls)
+
+# path("retrieve-stats", views.stats, name="stats"),
+#     path("add-correct", views.add_correct, name="add-correct"),
+#     path("add-incorrect", views.add_incorrect, name="add-incorrect"),
+#     path("change-points", views.change_points, name="change-points"),
+#     path("retrieve-question", views.retrieve, name="retrieve"),
+#     path("retrieve-others", views.retrieve_others, name="retrieve-others")
