e26f1b6514e13a5260fdff5220c103ae4f30a461
p3rplexed _ <102179895+Js-cook@users.noreply.github.com>
2022-06-04 01:24:12 +0000

    ADD - Added login and logout functionality

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
-    path("practice/<str:declension>/", views.question, name="question")
+    path("practice/<str:declension>/", views.question, name="question"),
+    path("logout", views.logout_request, name="log_out")
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def index(request):
   return render(request, "frontend/index.html")
@@ -8,3 +9,7 @@ def index(request):
 def question(request, declension):
   user = request.user
   return render(request, "frontend/question.html", {"user": user, "declension": declension})
+
+def logout_request(request):
+  logout(request)
+  return redirect("index")
