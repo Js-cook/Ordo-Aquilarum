1070e3222e4527f853e163889875a68499366125
p3rplexed _ <102179895+Js-cook@users.noreply.github.com>
2022-06-06 17:33:54 +0000

    ADD - Added menu items and icons

--- frontend/views.py
+++ frontend/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from api.models import UserExtension
 # Create your views here.
 def index(request):
-  return render(request, "frontend/index.html")
+  user = request.user
+  if user.is_authenticated:
+    user_extension = UserExtension.objects.get(username=request.user.username)
+    return render(request, "frontend/index.html", {"usern": user_extension})
+  else:
+    return redirect("login")
 
 @login_required
 def question(request, declension):
