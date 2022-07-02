608cf0f800a6a351c8c2de53704d288357d9b3b5
p3rplexed _ <102179895+Js-cook@users.noreply.github.com>
2022-07-02 18:23:41 +0000

    ADD - Added another base template because reasons

--- frontend/static/frontend/rumble.js
+++ frontend/static/frontend/rumble.js
@@ -1,7 +1,11 @@
 window.onload = function(){
   if (window.sessionStorage("firstExecute") === null){
+    console.log("yes")
     document.getElementById("start-modal").click()
 
     window.sessionStorage("firstExecute", false)
+  }
+  else {
+    console.log("no")
   }
 }
