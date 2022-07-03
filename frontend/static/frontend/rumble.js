97e0d4ee6d76ca82d2e846dc56c3f3623edba153
p3rplexed _ <102179895+Js-cook@users.noreply.github.com>
2022-07-03 21:16:29 +0000

    ADD - Minor tweaks to base template for team competition

--- frontend/static/frontend/rumble.js
+++ frontend/static/frontend/rumble.js
@@ -1,11 +1,28 @@
+const loggedUser = document.getElementById("logged-user").innerHTML
+const teamList = document.getElementById("player-list")
+
 window.onload = function(){
   if (sessionStorage.getItem("firstExecute") === null){
     console.log("yes")
     document.getElementById("start-modal").click()
-
+    let url = `https://ordo-aquilarum.p3rplexed.repl.co/api/update-competing/${loggedUser}/true/`
+    fetch(url)
     sessionStorage.setItem("firstExecute", false)
   }
   else {
     console.log("no")
+  }
+}
+
+function joinTeam(team){
+  if (team == "blue"){
+   teamList.innerHTML += `
+    <li class="list-group-item list-group-item-primary">${loggedUser}</li>  
+    ` 
+  }
+  else {
+    teamList.innerHTML += `
+    <li class="list-group-item list-group-item-danger">${loggedUser}</li>
+    `
   }
 }
