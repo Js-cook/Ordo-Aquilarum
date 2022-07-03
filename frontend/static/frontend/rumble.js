2ac3eef879b652865d0176ff6a2fa10025ec8541
p3rplexed _ <102179895+Js-cook@users.noreply.github.com>
2022-07-03 21:21:16 +0000

    ADD - Added team selection functionality

--- frontend/static/frontend/rumble.js
+++ frontend/static/frontend/rumble.js
@@ -1,5 +1,7 @@
 const loggedUser = document.getElementById("logged-user").innerHTML
 const teamList = document.getElementById("player-list")
+const blueTeamBtn = document.getElementById("blue-team-btn")
+const redTeamBtn = document.getElementById("red-team-btn")
 
 window.onload = function(){
   if (sessionStorage.getItem("firstExecute") === null){
@@ -18,11 +20,18 @@ function joinTeam(team){
   if (team == "blue"){
    teamList.innerHTML += `
     <li class="list-group-item list-group-item-primary">${loggedUser}</li>  
-    ` 
+    `
+    blueTeamBtn.disabled = true
+    redTeamBtn.disabled = true
+    let url = `https://ordo-aquilarum.p3rplexed.repl.co/api/change-team/${loggedUser}/blue/`
   }
   else {
     teamList.innerHTML += `
     <li class="list-group-item list-group-item-danger">${loggedUser}</li>
     `
+    blueTeamBtn.disabled = true
+    redTeamBtn.disabled = true
+    let url = `https://ordo-aquilarum.p3rplexed.repl.co/api/change-team/${loggedUser}/red/`
   }
+  fetch(url)
 }
